696f85fbc4b66ffa80e2281e924609a38447ffca
Ondřej Nový <onovy@debian.org>
2018-09-05 13:57:35 +0200

    New upstream version 1.1.0

--- setup.py
+++ setup.py
@@ -1,64 +1,48 @@
 import io
-import re
 
-import os
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 
-with io.open('README.rst', 'rt', encoding='utf8') as f:
+with io.open("README.rst", "rt", encoding="utf8") as f:
     readme = f.read()
 
-with io.open(
-    os.path.join('pallets_sphinx_themes', '__init__.py'), 'rt', encoding='utf8'
-) as f:
-    version = re.search(
-        r'^__version__ = \'(.+)\'', f.read(), re.M
-    ).group(1)
-
 setup(
-    name='Pallets-Sphinx-Themes',
-    version=version,
-    url='https://github.com/pallets/pallets-sphinx-themes/',
-    license='BSD',
-    author='The Pallets Team',
-    author_email='contact@palletsprojects.com',
-    description='Sphinx themes for Pallets and related projects.',
+    name="Pallets-Sphinx-Themes",
+    version="1.1.0",
+    url="https://github.com/pallets/pallets-sphinx-themes/",
+    license="BSD",
+    author="The Pallets Team",
+    author_email="contact@palletsprojects.com",
+    description="Sphinx themes for Pallets and related projects.",
     long_description=readme,
-    packages=find_packages(),
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    install_requires=['sphinx'],
+    install_requires=["sphinx"],
     entry_points={
-        'sphinx.html_themes': [
-            'pocoo = pallets_sphinx_themes',
-            'flask = pallets_sphinx_themes',
-            'jinja = pallets_sphinx_themes',
-            'werkzeug = pallets_sphinx_themes',
-            'click = pallets_sphinx_themes',
-            'babel = pallets_sphinx_themes',
-            'platter = pallets_sphinx_themes',
-            'flask-sqlalchemy = pallets_sphinx_themes',
-        ],
-        'pygments.styles': [
-            'pocoo = pallets_sphinx_themes.pocoo:PocooStyle',
-            'jinja = pallets_sphinx_themes.jinja:JinjaStyle',
-        ],
+        "pygments.styles": [
+            "pocoo = pallets_sphinx_themes.themes.pocoo:PocooStyle",
+            "jinja = pallets_sphinx_themes.themes.jinja:JinjaStyle",
+        ]
     },
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Framework :: Sphinx',
-        'Framework :: Sphinx :: Theme',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Documentation',
-        'Topic :: Documentation :: Sphinx',
-        'Topic :: Software Development :: Documentation',
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Sphinx",
+        "Framework :: Sphinx :: Theme",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Documentation",
+        "Topic :: Documentation :: Sphinx",
+        "Topic :: Software Development :: Documentation",
     ],
 )
